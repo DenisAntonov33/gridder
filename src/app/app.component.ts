20e8d123ebcf6c68a3e16e0817f30b7ab30ab1fb
denis <danchic95@gmail.com>
2024-09-04 20:57:36 +0400

    fix: cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
 
--- src/app/pages/sign-up-page/sign-up.component.ts
+++ src/app/pages/sign-up-page/sign-up.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild} from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
 import {MatButton} from "@angular/material/button";
 import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
@@ -6,7 +6,6 @@ import {MatInput, MatInputModule} from "@angular/material/input";
 import {ExistingUsernameValidatorDirective} from "../../directives/existing-username/existing-username-validator";
 import {AuthService} from "../../services/auth/auth.service";
 import {Router} from "@angular/router";
-import {UserService} from "../../services/user/user.service";
 
 @Component({
   selector: 'app-sign-up-page',
